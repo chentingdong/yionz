6bfecaa6737fa499a53a79a33af6fefbc1b283d3
Tingdong Chen <chentingdong@gmail.com>
2023-05-10 20:26:08 +0800

    finally auto update for ui

--- app/v/page.tsx
+++ app/v/page.tsx
@@ -1,16 +1,16 @@
 import prisma from "@/prisma/prisma";
 
-export default function Page() {
-  const appState = getData()
-  console.log('appState', appState)
+export default async function Page() {
+  // const appState = await getData();
+  // console.log('appState', appState);
 
-  return <h1>Hello, Next.js!</h1>;
+  return <h1>Hello, Next.js!!</h1>;
 }
 
-const getData = async () => {
-  const appState = await prisma.appState.findMany();
-  return {
-    props: appState,
-  };
-};
+// const getData = async () => {
+//   const appState = await prisma.appState.findMany();
+//   return {
+//     props: appState,
+//   };
+// };
 
