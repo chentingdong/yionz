843bc15a4985d561731ec4ad3bd95bc71b200bb2
Tingdong Chen <chentingdong@gmail.com>
2023-05-13 21:58:58 +0800

    i18n

--- app/[lang]/[id]/page.tsx
+++ app/[lang]/[id]/page.tsx
@@ -1,13 +1,24 @@
 import AppSteps from "./appSteps";
+import { Locale } from '@/i18n/i18n-config';
+import { getTranslation } from "@/i18n/translations";
 import prisma from "@/prisma/prisma";
 
-export default async function Page({ params }) {
+export type PageProps = {
+  params: {
+    lang: Locale,
+    id: string,
+  };
+};
+
+export default async function Page({ params }: PageProps) {
   const artifact = await getData(params.id);
+  const translation = await getTranslation(params.lang);
+
   return (
     <div className="container d-flex flex-column h-100">
-      <label className="text-capitalize"><b>Video Story:</b> {artifact?.name}</label>
+      <label className="text-capitalize"><b>{translation.step1Story.title}:</b> {artifact?.name}</label>
       <div id="step-tabs" className="flex-grow-1 d-flex flex-column">
-        <AppSteps />
+        <AppSteps translation={translation} />
       </div>
     </div >
   );
