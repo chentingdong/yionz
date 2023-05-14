dd9d139ec0cd73b94b5d03af1f51e24213c3c847
Tingdong Chen <chentingdong@gmail.com>
2023-05-14 11:02:49 +0800

    step1 internationalization

--- app/[lang]/[id]/appSteps.tsx
+++ app/[lang]/[id]/appSteps.tsx
@@ -1,22 +1,25 @@
 "use client";
 
+import React, { useState } from "react";
+
 import MakeMovie from "./step3Movie";
 import MakeStory from "./step1Story";
-import React from "react";
 import ShowOff from "./step4BSO";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import VideoClips from "./step2Clips";
 
 export default function AppSteps({ translation }) {
+  const [key, setKey] = useState('step-1');
   return (
     <Tabs
-      className="d-flex justify-content-between"
+      className="d-flex justify-content-between mt-2"
       variant="pills"
-      activeKey='step-1'
+      activeKey={key}
+      onSelect={k => setKey(k)}
     >
       <Tab eventKey="step-1" title={translation.appSteps.makeStory}>
-        <MakeStory />
+        <MakeStory translation={translation} />
       </Tab>
       <Tab eventKey="step-2" title={translation.appSteps.videoClips}>
         <VideoClips />
