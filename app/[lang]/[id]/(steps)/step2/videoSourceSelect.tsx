1a430dec0e390ab72dc973ebb96ed17d27455c45
Tingdong Chen <chentingdong@gmail.com>
2023-06-15 20:01:15 +0800

    fix multiple tabs in one page

--- app/[lang]/[id]/(steps)/step2/videoSourceSelect.tsx
+++ app/[lang]/[id]/(steps)/step2/videoSourceSelect.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Nav, Tab } from 'react-bootstrap';
+import { Nav, Tab } from "react-bootstrap";
 
 import CreateVideo from "./video";
 import { Prisma } from "@prisma/client";
@@ -12,28 +12,83 @@ type Props = {
   translation: any;
 };
 
-export default function VideoSourceSelect({ clip, template, translation }: Props) {
+export default function VideoSourceSelect({
+  clip,
+  template,
+  translation,
+}: Props) {
   return (
     <div className="row">
-      <div className="col-2 nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-        <button className="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
+      <div
+        className="col-2 nav flex-column nav-pills"
+        id={`${clip.id}-tab`}
+        role="tablist"
+        aria-orientation="vertical"
+      >
+        <button
+          className="nav-link active"
+          id={`${clip.id}-video-tab`}
+          data-bs-toggle="pill"
+          data-bs-target={`#${clip.id}-video`}
+          type="button"
+          role="tab"
+          aria-controls={`${clip.id}-video`}
+          aria-selected="true"
+        >
           {translation.step2Clip.video}
         </button>
-        <button className="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">
+        <button
+          className="nav-link"
+          id={`${clip.id}-images-tab`}
+          data-bs-toggle="pill"
+          data-bs-target={`#${clip.id}-images`}
+          type="button"
+          role="tab"
+          aria-controls={`${clip.id}-images`}
+          aria-selected="false"
+        >
           {translation.step2Clip.images}
         </button>
-        <button className="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">
+        <button
+          className="nav-link"
+          id={`${clip.id}-animation-tab`}
+          data-bs-toggle="pill"
+          data-bs-target={`#${clip.id}-animation`}
+          type="button"
+          role="tab"
+          aria-controls={`${clip.id}-animation`}
+          aria-selected="false"
+        >
           {translation.step2Clip.animation}
         </button>
       </div>
-      <div className="col-10 tab-content" id="v-pills-tabContent">
-        <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-          <CreateVideo video={clip.video} template={template} translation={translation} />
+      <div className="col-10 tab-content" id={`${clip.id}-tabContent`}>
+        <div
+          className="tab-pane fade show active"
+          id={`${clip.id}-video`}
+          role="tabpanel"
+          aria-labelledby={`${clip.id}-video-tab`}
+        >
+          <CreateVideo
+            video={clip.video}
+            template={template}
+            translation={translation}
+          />
         </div>
-        <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+        <div
+          className="tab-pane fade"
+          id={`${clip.id}-images`}
+          role="tabpanel"
+          aria-labelledby={`${clip.id}-images-tab`}
+        >
           Images
         </div>
-        <div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+        <div
+          className="tab-pane fade"
+          id={`${clip.id}-animation`}
+          role="tabpanel"
+          aria-labelledby={`${clip.id}-animation-tab`}
+        >
           Animation
         </div>
       </div>
