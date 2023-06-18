e4775a9e1290ff7ba811aa14a7a6ec2f46c94f52
Tingdong Chen <chentingdong@gmail.com>
2023-06-18 13:33:41 +0800

    use graphicmagick to resize/crop image

--- app/[lang]/[id]/(steps)/step2/images.tsx
+++ app/[lang]/[id]/(steps)/step2/images.tsx
@@ -1,10 +1,12 @@
-import { Prisma, Template } from "@prisma/client";
+import { Image, Template } from "@prisma/client";
 import SortableList, { SortableItem } from "react-easy-sort";
+import { deleteImage, uploadImage } from "./actions";
 
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import { FileUploader } from "react-drag-drop-files";
+import Loading from "@/app/components/loading";
 import React from "react";
 import arrayMove from "array-move";
-import { uploadImage } from "./actions";
 
 type Props = {
   images: Image[];
@@ -14,22 +16,24 @@ type Props = {
   translation: any;
 };
 
-
 export default function CreateImages({ images, artifactId, clipId }: Props) {
-  const fileTypes = ['jpg', 'jpeg', 'png'];
+  const fileTypes = ["jpg", "jpeg", "png"];
   const [items, setItems] = React.useState(images);
+  const [loading, setLoading] = React.useState(false);
 
   const updateImages = (fileList: File[]) => {
+    setLoading(true);
     let order = images.length + 1;
     Array.from(fileList).forEach(async (file: File) => {
       const formData = new FormData();
-      formData.append('file', file);
-      formData.append('artifactId', artifactId);
-      formData.append('clipId', clipId);
-      formData.append('order', order.toString());
-      uploadImage(formData);
+      formData.append("file", file);
+      formData.append("artifactId", artifactId);
+      formData.append("clipId", clipId);
+      formData.append("order", order.toString());
+      await uploadImage(formData);
       order++;
     });
+    setLoading(false);
   };
 
   const onSortEnd = (oldIndex: number, newIndex: number) => {
@@ -38,26 +42,39 @@ export default function CreateImages({ images, artifactId, clipId }: Props) {
 
   return (
     <div>
-      <FileUploader
-        multiple={true}
-        handleChange={updateImages}
-        name="file"
-        types={fileTypes}
-      />
+      <div className="row mx-2">
+        <div className="col-11">
+          <FileUploader
+            multiple={true}
+            handleChange={updateImages}
+            name="file"
+            types={fileTypes}
+          />
+        </div>
+        <div className="col-1">
+          {loading && <Loading size={24} />}
+        </div>
+      </div>
       <SortableList
         onSortEnd={onSortEnd}
         className="d-flex flex-wrap user-select-none"
-        draggedItemClassName="opacity-50 shadow-lg"
+        draggedItemClassName="opacity-75 shadow-lg"
       >
         {items.map((item, index) => (
           <SortableItem key={index}>
-            <div className="flex-shrink-0 display-flex m-2 cursor-move user-select-none">
+            <div className="flex-shrink-0 display-flex m-2 cursor-move user-select-none position-relative">
               <img
                 className="pe-none"
-                // alt={item.order.toString()}
+                alt={item.order.toString()}
                 src={item.url}
-                style={{ width: 150, height: 'auto' }}
+                style={{ width: 150, height: "auto" }}
               />
+              <button
+                className="btn btn-link position-absolute top-0 end-0 px-1 py-0"
+                onClick={() => deleteImage(item)}
+              >
+                <AiOutlineCloseCircle />
+              </button>
             </div>
           </SortableItem>
         ))}
