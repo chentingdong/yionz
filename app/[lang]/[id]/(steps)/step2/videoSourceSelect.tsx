1c49fba2727878346791b31a66b1cc351f7fa365
Tingdong Chen <chentingdong@gmail.com>
2023-06-23 16:17:33 +0800

    videoSourceSelector use dynamic json key for tab.nav

--- app/[lang]/[id]/(steps)/step2/videoSourceSelect.tsx
+++ app/[lang]/[id]/(steps)/step2/videoSourceSelect.tsx
@@ -84,7 +84,7 @@ export default function VideoSourceSelect({
             aria-selected={active === tab.nav}
             onClick={setVideoSource}
           >
-            {translation.step2Clip.video}
+            {translation.step2Clip[tab.nav]}
           </button>
         )}
       </div>
